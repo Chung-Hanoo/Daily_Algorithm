77d49f90ec8293f2f29a8cd21260bf7273597e88
hanoo <woo980711@naver.com>
2022-02-23 15:00:43 +0900

    Create repositories beakjoon 10926 commit

---
+++ src/num_10926.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class num_10926 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 아이디 입력
+        String ID = bf.readLine();
+
+        // 출력
+        bw.write(ID + "??!");
+        bw.flush();
+        bw.close();
+    }
+}
